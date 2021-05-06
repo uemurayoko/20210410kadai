028fbac07da83df6915ae486fdf02c43bf168754
uemurayoko <y.uemura11@gmail.com>
2021-05-06 23:45:19 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+
+let strElem = document.getElementById('str');
+
+console.log(strElem);
+
+let str = 'バリバリ最強No.1';
+strElem.innerHTML = str;
+
+let FamiliyName = "生ハム";
+let lastName = "メロン";
+
+console.log(FamiliyName + lastName);
+console.log(4643+ 6); 
+
+console.log('No.' + 1);
+console.log('"32"' + 1);
